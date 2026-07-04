c6d02aadb4ae4a53e26ff100a8255dc610ed541f
Ayusman Nanda <abhiseknanda799@gmail.com>
2026-07-04 16:08:25 +0530

    backend: fix build and deployment failure.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, HashRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const Router =
+    window.location.protocol === "file:" ? HashRouter : BrowserRouter;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <Router>
             <App />
-        </BrowserRouter>
-    </React.StrictMode>,
-)
+        </Router>
+    </React.StrictMode>
+);
